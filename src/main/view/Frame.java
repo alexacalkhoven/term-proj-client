565354c9f2233c400199574241c7b68e6986de55
Alexa Calkhoven <alexa.calkhoven1@ucalgary.ca>
2020-04-08 14:40:27 -0600

    LoginPanel and Frame classes started.

--- src/main/view/Frame.java
+++ src/main/view/Frame.java
@@ -1,5 +1,28 @@
 package main.view;
 
-public class Frame {
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+//shows the various views
+@SuppressWarnings("serial")
+public class Frame extends JFrame{
+	
+	public Frame(String s) {
+		super(s);
+		setTitle("Course Registration System");
+		setLayout(new BorderLayout());
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+		setResizable(false);
+		pack();
+		Dimension dimensions = Toolkit.getDefaultToolkit().getScreenSize();
+		setLocation((dimensions.width - getSize().width) / 2, (dimensions.height - getSize().height) / 2);
+	}
+	
+	public void setPanel(JPanel p) {
+		this.setContentPane(p);
+	}
 }
--- src/main/view/LoginPanel.java
+++ src/main/view/LoginPanel.java
@@ -1,5 +1,58 @@
 package main.view;
 
-public class LoginPanel {
+import java.awt.event.ActionEvent;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class LoginPanel extends JPanel {
+
+	private JButton loginStudent;
+	private JButton loginAdmin;
+	private JButton quit;
+	
+	public LoginPanel(){
+		setupButtons();
+		addButtons();
+	}
+
+	private void addButtons() {
+		this.add(loginStudent);
+		this.add(loginAdmin);
+		this.add(quit);
+	}
+
+	private void setupButtons() {
+		setupStudent();
+		setupAdmin();
+		setupQuit();
+	}
+
+	private void setupQuit() {
+		quit = new JButton("Quit");
+		quit.addActionListener((ActionEvent e) -> {
+			System.out.println("Quit");
+		});
+	}
+
+	private void setupAdmin() {
+		loginAdmin = new JButton("Admin");
+		loginAdmin.addActionListener((ActionEvent e) -> {
+			System.out.println("Admin");
+		});
+	}
+
+	private void setupStudent() {
+		loginStudent = new JButton("Student");
+		loginStudent.addActionListener((ActionEvent e) -> {
+			System.out.println("Student");
+		});
+	}
+	
+	public static void main(String [] args) {
+		JPanel p = new LoginPanel();
+		Frame myFrame = new Frame("Hey");
+		myFrame.setPanel(p);
+	}
 }
