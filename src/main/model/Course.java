9b8fd006922238e2a1c8848d637c76550603841f
raduschirliu <radu.schirliu@gmail.com>
2020-04-10 19:52:57 -0600

    Added models to client

---
+++ src/main/model/Course.java
@@ -0,0 +1,60 @@
+package main.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Course implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+	private int number;
+	private ArrayList<Course> preReq;
+	private ArrayList<CourseOffering> offeringList;
+
+	public Course(String name, int num) {
+		setName(name);
+		setNumber(num);
+
+		preReq = new ArrayList<Course>();
+		offeringList = new ArrayList<CourseOffering>();
+	}
+
+	public CourseOffering getCourseOffering(int section) {
+		for (CourseOffering o : offeringList) {
+			if (o.getSecNum() == section)
+				return o;
+		}
+
+		return null;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String courseName) {
+		this.name = courseName;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int courseNum) {
+		this.number = courseNum;
+	}
+
+	public String getFullName() {
+		return getName() + " " + getNumber();
+	}
+
+	public String toString() {
+		String s = "Course: " + getName() + " " + getNumber() + "\nAll course sections:\n";
+
+		for (CourseOffering c : offeringList) {
+			s += c + "\n";
+		}
+
+		return s;
+	}
+}
--- src/main/model/Student.java
+++ src/main/model/Student.java
@@ -1,5 +1,72 @@
 package main.model;
 
-public class Student {
+import java.io.Serializable;
+import java.util.ArrayList;
 
+public class Student implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+	private int id;
+	private ArrayList<Registration> registrationList;
+
+	public Student(String name, int id) {
+		registrationList = new ArrayList<Registration>();
+		setName(name);
+		setId(id);
+	}
+
+	public void addRegistration(Registration registration) {
+		registrationList.add(registration);
+	}
+
+	public boolean isRegistered(Course course) {
+		for (Registration reg : registrationList) {
+			if (reg.getOffering().getCourse().equals(course))
+				return true;
+		}
+
+		return false;
+	}
+
+	public void removeRegistration(Course course) {
+		for (Registration reg : registrationList) {
+			if (reg.getOffering().getCourse().equals(course)) {
+				System.out.println("Dropped course: " + course.getFullName());
+				reg.removeRegistration();
+				registrationList.remove(reg);
+				return;
+			}
+		}
+
+		System.err.println("Error, student not registered for " + course.getFullName());
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String studentName) {
+		this.name = studentName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int studentId) {
+		this.id = studentId;
+	}
+
+	public String toString() {
+		String s = "Student name: " + getName() + ", Student ID: " + getId() + "\n";
+
+		s += "Registered courses:\n";
+
+		for (Registration reg : registrationList) {
+			s += "\n\n" + reg;
+		}
+
+		return s;
+	}
 }
